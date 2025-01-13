d16d0429375ba2b1f130ffe1698be73a2d88c03c
mosesgitonga <infosec947@gmail.com>
2025-01-13 20:12:56 +0300

    update: engine

--- models/engine.py
+++ models/engine.py
@@ -1,6 +1,34 @@
+from sqlalchemy import create_engine, Table, func, or_ 
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm.exc import NoResultFound
+import load_dotenv
+import os 
+import logging 
+
+logging.basicConfig(level=logging.INFO)
+load_dotenv()
+
 class Storage():
     def __init__(self):
-        development_db = ""
-        production_db = ""
+    
+        self.production_db = ""
+        if os.getenv('dev_env') = 'development_env':
+            self.db_uri = os.getenv('CROPSHIELD_DEVELOPMENT_DB')
+        else:
+            self.db_uri = os.getenv('CROPSHIELD_PRODUCTION_DB')
+
+        self.engine = create_engine(
+            self.db_uri,
+            pool_size=20,
+            max_overflow=30,
+            pool_timeout=25,
+            pool_recycle=3600
+        )
+
+        self.Session = scoped_session(sessionmaker(bind=self.engine, expire_on_commit=False))
+
+    @contextmanager
+    def get_session(self):
+        pass
 
         
