79f65a39f2bf8852c150456696f129aa962fcf93
Moses <122813387+mosesgitonga@users.noreply.github.com>
2025-06-19 15:13:52 +0300

    Update model.py

--- backend/model.py
+++ backend/model.py
@@ -4,7 +4,7 @@ from torchvision import models
 MODEL_PATH = "../AI/nucleusV5_model.pth"
 
 def load_model():
-    with torch.serialization.safe_globals([models.resnet34]):  # <--- Allow only trusted classes
+    with torch.serialization.safe_globals([models.resnet34]):  
         model = torch.load(MODEL_PATH, map_location=torch.device("cpu"), weights_only=False)
     model.eval()
     return model
